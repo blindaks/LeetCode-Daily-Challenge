38cd43b153ec58007293eaf6b8744bd9f4ad3bca
Akrati Verma <128223364+blindaks@users.noreply.github.com>
2023-10-05 14:31:11 +0530

    05 Oct 2023

---
+++ Majority Element II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        mp[nums[i]]++;
+        for(auto it:mp)
+        {
+            if(it.second>nums.size()/3)
+            ans.push_back(it.first);
+        }
+        return ans;
+    }
+};

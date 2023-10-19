ad2a9f744543f12fe58577aec9f093c53ab9f3b2
Akrati Verma <128223364+blindaks@users.noreply.github.com>
2023-10-19 17:12:20 +0530

    19 Oct 2023

---
+++ Backspace String Compare.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1,s2;
+        for(char ch:s)
+        {
+            if(ch=='#')
+            {
+                if(!s1.empty())
+                s1.pop();
+            }
+            else
+            s1.push(ch);
+        }
+        for(char ch:t)
+        {
+            if(ch=='#')
+            {
+                if(!s2.empty())
+                s2.pop();
+            }
+            else
+            s2.push(ch);
+        }
+        return s1==s2;
+    }
+};
